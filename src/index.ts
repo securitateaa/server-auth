f59a8a3d5a3a490c2d985410f9f957b7186fe4ce
George <george.bara@ivfa.ro>
2024-10-22 17:51:29 +0300

    added cors origin for front-end client

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import dotenv from "dotenv";
 import express, { Express } from "express";
 import http from "http";
+const cors = require("cors");
 import AuthRoutes from "./routes/auth";
 
 dotenv.config();
 
-const index: Express = express();
-const server = http.createServer(index);
+const app: Express = express();
+const server = http.createServer(app);
+app.use(
+  cors({
+    origin: process.env.CLIENT_ADDRESS,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
-index.use(express.json());
-index.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-index.use("/auth", AuthRoutes);
+app.use("/auth", AuthRoutes);
 
 (async () => {
   try {
